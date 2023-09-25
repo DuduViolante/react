3773a8b49caab9049422506b0d97b64bc13afbc8
DuduViolante <126472870+DuduViolante@users.noreply.github.com>
2023-09-25 11:52:40 -0300

    aula dia 25

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
@@ -8,7 +8,7 @@ import EditarProdutos from './routes/EditarProdutos.jsx';
 import Erro404 from './routes/Erro404.jsx';
 import Produtos from './routes/Produtos.jsx';
 import ExcluirProduto from './routes/ExcluirProduto.jsx';
-import AdicionarProdutos from './routes/components/AdicionarProdutos.jsx';
+import InserirProduto from './routes/InserirProduto.jsx';
 
 const router = createBrowserRouter([
   {path: '/', element: <App/>,
@@ -18,26 +18,14 @@ const router = createBrowserRouter([
       {path: '/produtos', element: <Produtos/>},
       {path: '/editar/produtos/:id', element: <EditarProdutos/>},
       {path: '/excluir/produtos/:id', element: <ExcluirProduto/>},
-      
-    ],
-  },
-  {path: '/adicionar/produtos', element: <AdicionarProdutos/>}
+      {path: '/cadastrar/produto', element: <InserirProduto/>},
+    ]
+  }
 ]);
 
-
-// children: [
-//   { path: '/', element: <Home /> },
-//   { path: '/produtos', element: <Produtos /> },
-//   { path: '/editar/produtos/:id', element: <EditarProduto /> },
-//   { path: '/excluir/produtos/:id', element: <ExcluirProduto /> },
-// ],
-// },
-// { path: '/adicionar/produtos', element: <AdicionarProduto /> },
-// ]);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
        <RouterProvider router={router}/>
-  </React.StrictMode>,
+  ,
 )
 
